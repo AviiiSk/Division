7ff1e1f3a1bdce885c616a489cfe2a2ed2475c2c
Avesh Shaikh <97118756+AviiiSk@users.noreply.github.com>
2023-01-16 02:10:29 +0530

    Add files via upload
    Division program

---
+++ Division.java
@@ -0,0 +1,33 @@
+
+
+// 4. Write the java program to design method for division of two number,
+//which returns int value to that method and result should be print into main method.
+
+
+
+
+
+
+public class Division {
+
+	
+		@SuppressWarnings("unused")
+		public static void subtract(int a, int b)
+		{
+			int c  = a/b;
+			
+			System.out.println("Multiplication of Two Numbers : " + c);
+			
+			
+		}
+		
+
+		public static void main(String[] args) {
+			
+			subtract(30, 15);
+			
+			
+
+		}
+
+	}
